3e8550a1ceb920934b11f82a7017bdf66ff1e3a3
Josh Schaben <jschaben42@gmail.com>
2019-11-19 17:49:43 -0600

    fixed but with img alt name

--- src/components/card.js
+++ src/components/card.js
@@ -3,7 +3,7 @@ import React from "react";
 const Card = props => {
     return (
         <div key={props.id}>
-          <img alt="NASA Image of the Day" src={props.url} />
+          <img alt="NASA Random Pic of the Day" src={props.url} />
           <h2>{props.title}</h2>
           <h3>{props.date}</h3>
           <p>{props.explanation}</p>
