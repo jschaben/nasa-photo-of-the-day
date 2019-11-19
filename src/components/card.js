3abde1119051d1c8153731145248e1910c256c08
Josh Schaben <jschaben42@gmail.com>
2019-11-19 17:13:49 -0600

    setting up components and working on cards

---
+++ src/components/card.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Card = props => {
+    return (
+        <div key={props.id}>
+          <img alt="NASA Image of the Day" src={props.url} />
+          <h2>{props.title}</h2>
+          <h3>{props.date}</h3>
+          <p>{props.explanation}</p>
+        </div>
+    );
+};
+
+export default Card;
