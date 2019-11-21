e18b96b4e4ae69b26c15d91da32fe28b8572d8ff
Josh Schaben <jschaben42@gmail.com>
2019-11-21 16:56:06 -0600

    working on button

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import React from "react";
 import "./App.css";
 import CardDisplay from "./components/CardDisplay";
 import "bootstrap/dist/css/bootstrap.min.css";
+import {Button} from "reactstrap";
 
 function App() {
+
+  
+
   return (
     <div className="App">
       <h1>Random NASA Photo of the Day</h1>
--- src/components/card.js
+++ src/components/card.js
@@ -2,13 +2,17 @@ import React from "react";
 import { Card as Cardstyle} from "reactstrap";
 import {Container} from "reactstrap";
 import {CardTitle, CardSubtitle} from "reactstrap";
-
+import {Button} from "reactstrap";
 const Cardset = props => {
-    return (
+  function refreshPage(){
+    window.location.reload(false);
+  }  
+  return (
       <Container key={props.id}>
       <Cardstyle color="dark" className="text-white">
         <h3>{props.date}</h3>
         <h2>{props.title}</h2>
+        <Button onClick={refreshPage}> New Image </Button>
         <img alt="Random NASA pic of the day" src={props.url} />
         <hr></hr>
         <CardSubtitle>{props.explanation}</CardSubtitle>
